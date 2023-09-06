9c002c1075f9b9edfc053f9a44f6d0d214a423b2
Aqib Ali <101803091+aqibalisy@users.noreply.github.com>
2023-09-07 02:21:28 +0530

    A website fo drum beats:)

---
+++ index.js
@@ -0,0 +1,67 @@
+// let buttonB = ()=>{
+//    alert("hello world");
+// }
+
+// let buttonLength = document.querySelectorAll(".drum").length;
+
+for (let i=0; i<document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+       let keyInnerHTML = this.innerHTML;
+      keyDown(keyInnerHTML);
+      buttonAnimation(keyInnerHTML); //
+       
+    });
+}
+ document.addEventListener("keydown",(event)=>{
+    keyDown(event.key);
+    buttonAnimation(event.key)
+ })
+
+ function keyDown(evnt){
+    switch (evnt){ 
+        case "w": 
+        let tom1 = new Audio("./sounds/tom-1.mp3")  
+        tom1.play();
+            break;
+            case "a": 
+       let tom2 = new Audio("./sounds/tom-2.mp3")  
+            tom2.play();
+                break;
+                case "s": 
+        let tom3 = new Audio("./sounds/tom-3.mp3")  
+        tom3.play();
+            break;
+            case "d": 
+        let tom4 = new Audio("./sounds/tom-4.mp3")  
+        tom4.play();
+            break;
+
+            case "j": 
+        let snare = new Audio("./sounds/snare.mp3")  
+        snare.play();
+            break;
+            case "k":
+        let crash = new Audio("./sounds/crash.mp3")  
+        crash.play();
+            break;
+            case "l":
+        let kick = new Audio("./sounds/kick-bass.mp3")  
+        kick.play();
+            break;
+
+        default: console.log(evnt)
+            
+       }
+ }
+
+        function buttonAnimation(key) {
+            document.querySelector("."+key).classList.add("pressed")
+            
+            setTimeout(function() {
+                document.querySelector("."+key).classList.remove("pressed")
+                document.querySelector("."+key).classList.remove("pressed")
+            }, 100);
+        }
+
+
+        
